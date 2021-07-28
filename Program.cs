48d2a0da8f72e5d310eadc3ebf47372a00ad2b88
Pedro Villalta <pavblogs@gmail.com>
2021-07-28 11:23:28 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Primer_proyecto_de_consola_vcsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primer proyecto subido a versionador Git");
+
+
+
+        }
+    }
+}
