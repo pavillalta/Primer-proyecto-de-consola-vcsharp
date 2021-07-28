dd5d92b9f321b8e0fc6a1237af2fedbaf64d2e7f
Pedro Villalta <pavblogs@gmail.com>
2021-07-28 11:40:15 -0600

    commit

--- Program.cs
+++ Program.cs
@@ -8,7 +8,11 @@ namespace Primer_proyecto_de_consola_vcsharp
         {
             Console.WriteLine("Primer proyecto subido a versionador Git y clonado y actualizado");
 
-
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
 
         }
     }
